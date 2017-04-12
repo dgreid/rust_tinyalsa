9f8bacf78e594a3f21f0a86f07cde412b1595651
Dylan Reid <dgreid@chromium.org>
2017-04-11 17:29:14 -0700

    store min/max

--- src/pcm.rs
+++ src/pcm.rs
@@ -18,6 +18,8 @@ pub struct PcmConfig<'a> {
     card: AlsaCardIndex,
     device: AlsaDeviceIndex,
     direction: Direction,
+    min_rate: AudioRateHertz,
+    max_rate: AudioRateHertz,
     sample_rate: AudioRateHertz,
     params: &'a mut ffi::PcmParams,
 }
@@ -34,22 +36,20 @@ impl<'a> PcmConfig<'a> {
             Direction::Input => ffi::PCM_IN,
             Direction::Output => ffi::PCM_OUT,
         };
-        let get_result = unsafe { ffi::pcm_params_get(card, device, flags) };
-        match get_result {
-            None => Err(Error::CheckingParams),
-            Some(params) => Ok(PcmConfig { card: card,
-                                           device: device,
-                                           direction: direction,
-                                           sample_rate: 0,
-                                           params: params }),
-        }
+        let mut params = match unsafe { ffi::pcm_params_get(card, device, flags) } {
+            None => { return Err(Error::CheckingParams); },
+            Some(p) => p,
+        };
+
+        let min = unsafe { ffi::pcm_params_get_min(params, ffi::PcmParam::Rate) };
+        let max = unsafe { ffi::pcm_params_get_max(params, ffi::PcmParam::Rate) };
+
+        Ok(PcmConfig { card: card, device: device, direction: direction,
+                       min_rate: min, max_rate: max, sample_rate: 0, params: params })
     }
 
     pub fn set_rate(&'a mut self, rate: AudioRateHertz) -> Result<&'a mut PcmConfig, Error> {
-        let min = unsafe { ffi::pcm_params_get_min(self.params, ffi::PcmParam::Rate) };
-        let max = unsafe { ffi::pcm_params_get_max(self.params, ffi::PcmParam::Rate) };
-
-        if rate < min || rate > max {
+        if rate < self.min_rate || rate > self.max_rate {
             Err(Error::InvalidSampleRate)
         } else {
             self.sample_rate = rate;
